5821fffd798a57c8cc0a7b46c61eaabe5197d4c3
danilobatson <69876068+danilobatson@users.noreply.github.com>
2025-07-10 11:53:50 -0700

    refactor: remove unused font imports from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AnimatePresence } from 'framer-motion'
 import theme from '../styles/theme'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
+
 
 function MyApp({ Component, pageProps, router }) {
   return (
